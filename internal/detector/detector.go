33074419c34449954af138c6fd36a5a2f388210c
IVAN-GUY <kaluginivan2002@mail.ru>
2026-03-22 21:10:52 +0300

    add more sinature for detector

--- internal/detector/detector.go
+++ internal/detector/detector.go
@@ -13,22 +13,19 @@ type Detector struct {
 func NewDetector() *Detector {
 	return &Detector{
 		Signatures: []Signature{
-			{
-				Name:    "Nop sled",
-				Pattern: []byte{0x90, 0x90, 0x90, 0x90, 0x90},
-			},
-			{
-				Name:    "MZ Header",
-				Pattern: []byte("MZ"),
-			},
-			{
-				Name:    "PowerShell",
-				Pattern: []byte("powershell"),
-			},
-			{
-				Name:    "CMD",
-				Pattern: []byte("cmd.exe"),
-			},
+			{Name: "Nop sled", Pattern: []byte{0x90, 0x90, 0x90, 0x90, 0x90}},
+			{Name: "MZ Header", Pattern: []byte("MZ")},
+			{Name: "PE Header", Pattern: []byte{0x50, 0x45, 0x00, 0x00}},
+			{Name: "PowerShell", Pattern: []byte("powershell")},
+			{Name: "CMD", Pattern: []byte("cmd.exe")},
+			{Name: "HTTP URL", Pattern: []byte("http://")},
+			{Name: "HTTPS URL", Pattern: []byte("https://")},
+			{Name: "Base64", Pattern: []byte("base64")},
+			{Name: "VirtualAlloc", Pattern: []byte("VirtualAlloc")},
+			{Name: "WriteProcessMemory", Pattern: []byte("WriteProcessMemory")},
+			{Name: "CreateRemoteThread", Pattern: []byte("CreateRemoteThread")},
+			{Name: "LoadLibrary", Pattern: []byte("LoadLibrary")},
+			{Name: "IsDebuggerPresent", Pattern: []byte("IsDebuggerPresent")},
 		},
 	}
 }
